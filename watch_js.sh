90e9c0e64da880ad5202154b5c1438b39887397f
Steve Johnson <steve@graffitilabs.com>
2012-11-11 16:27:58 -0800

    Remove backbone from watch_js

--- watch_js.sh
+++ watch_js.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 coffee -o js/coffee -c coffee
-uglifyjs2 js/jquery-1.8.2.js js/underscore-1.4.2.js js/backbone-0.9.2.js js/bootstrap-just-popovers.js js/coffee/* -o js/gen/packed.js --beautify
+uglifyjs2 js/jquery-1.8.2.js js/underscore-1.4.2.js js/bootstrap-just-popovers.js js/coffee/* -o js/gen/packed.js --beautify
