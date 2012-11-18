84857104c55a4e26faad2abbe5cc640545d205b1
Steve Johnson <steve@graffitilabs.com>
2012-11-17 17:14:37 -0800

    Screwing around with doc styles

---
+++ code_samples/options.js
@@ -0,0 +1,6 @@
+$('.literally').literallycanvas({
+  backgroundColor: 'rgb(255, 0, 0)', // default rgb(230, 230, 230)
+  keyboardShortcuts: false,          // default true
+  sizeToContainer: false,            // default true
+  toolClasses: [LC.Pencil]           // see coffee/tools.coffee
+});
