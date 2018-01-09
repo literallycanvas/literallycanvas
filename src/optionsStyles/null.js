81076654ee6946624b462fac000aabb463bdd1f7
decaffeinate <sjohnson@hipmunk.com>
2018-01-09 11:34:18 -0800

    decaffeinate: Run post-processing cleanups on LiterallyCanvas.coffee and 42 other files

--- src/optionsStyles/null.js
+++ src/optionsStyles/null.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const DOM = require('../reactGUI/ReactDOMFactories-shim');
 const createReactClass = require('../reactGUI/createReactClass-shim');
 const {defineOptionsStyle} = require('./optionsStyles');
